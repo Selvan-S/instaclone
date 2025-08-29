03a2083a78eca06cb7c7eeff1f3f0aad3d2015ad
selvandevbee <selvan.s@dev-bee.com>
2025-08-29 13:06:54 +0530

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $connection = 'mongodb';
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'username',
+        'email',
+        'password',
+        'profile_image',
+        'bio',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
